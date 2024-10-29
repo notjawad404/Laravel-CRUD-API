8ec25092038ec2813c700560cb9d9f074dad06e5
jawadali000123 <jawadali000123@gmail.com>
2024-10-29 18:59:14 +0000

    Setup mysql database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/db', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['status' => 'success', 'message' => 'Database connection is working!']);
+    } catch (\Exception $e) {
+        return response()->json(['status' => 'error', 'message' => 'Database connection failed!', 'error' => $e->getMessage()]);
+    }
 });
