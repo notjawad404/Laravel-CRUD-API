b6ad5d73244de91be88bff68878244c6a66f3df8
jawadali000123 <jawadali000123@gmail.com>
2024-10-30 18:40:53 +0000

    Setup api

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,3 @@ Route::get('/', function () {
 
 
 
-Route::get('/db', function () {
-    try {
-        DB::connection()->getPdo();
-        return response()->json(['status' => 'success', 'message' => 'Database connection is working!']);
-    } catch (\Exception $e) {
-        return response()->json(['status' => 'error', 'message' => 'Database connection failed!', 'error' => $e->getMessage()]);
-    }
-});
